af176ab0f97f9d8ecaeba236bd158d58e5b45105
Joe Letner <jrletner@gmail.com>
2023-06-12 21:34:40 -0500

    liquor service

--- src/app/liquor/liquor.component.ts
+++ src/app/liquor/liquor.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { LiquorService } from '../services/liquor.service';
 
 @Component({
   selector: 'app-liquor',
   templateUrl: './liquor.component.html',
-  styleUrls: ['./liquor.component.scss']
+  styleUrls: ['./liquor.component.scss'],
 })
 export class LiquorComponent {
+  liquors: any = [];
+  constructor(private liquorService: LiquorService) {}
 
+  ngOnInit(): void {
+    this.liquorService.fetchLiquors().subscribe((res: any) => {
+      console.log(res);
+      if (res.success) {
+        this.liquors = res.payload.liquor;
+      }
+    });
+  }
 }
--- src/app/services/liquor.service.ts
+++ src/app/services/liquor.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const URL = 'http://localhost:3000/api/v1';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LiquorService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  fetchLiquors() {
+    const headers = {
+      Authorization: `Bearer ${environment.Token}`,
+      'Content-Type': 'application/json',
+    };
+
+    const options = {
+      headers: headers,
+    };
+
+    return this.http.get(`${URL}/liquors/index`, options);
+  }
 }
